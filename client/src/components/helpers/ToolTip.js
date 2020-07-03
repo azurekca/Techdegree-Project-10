225c4be1c4fee70d8597d7bce90c63242b750e98
Agata Zurek <agata.w.zurek@gmail.com>
2020-07-02 19:10:04 -0700

    added tooltip component

---
+++ client/src/components/helpers/ToolTip.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+/**
+ * ToolTip component to be used to present helpful information on click.
+ * Area where information will be added has a role of status that
+ * will notify assistive technologies when content updates.
+ * type="button" attribution on the button is to stop some browsers mistaking the button for a submit button
+ * Adapted from https://inclusive-components.design/tooltips-toggletips/
+ */
+function Tooltip({message}) {
+  const [tip, setTip] = useState();
+  const [spanTipClass, setSpanTipClass] = useState();
+
+  function showTip() {
+    setTip(message);
+    setSpanTipClass('tooltip--bubble');
+  }
+
+  function hideTip() {
+    setTip(null);
+    setSpanTipClass(null);
+  }
+
+  return (
+    <span className="tooltip--container">
+      <button type="button" aria-label="more info" onClick={showTip} onBlur={hideTip} onKeyDown={hideTip}>i</button>
+      <span role="status" className={spanTipClass}>{tip}</span>
+    </span>
+  );
+}
+
+export default Tooltip;
